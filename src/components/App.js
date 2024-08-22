9b9892e9528ab9f3d19fd676cb842f1f7b5e453a
sandybhatta <sandipresponse256@gmail.com>
2024-08-22 18:58:53 +0530

    Update App.js
    assignment of button counter completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[count,setcount]=useState(0)
+  function buttonClicked(){
+    setcount(count++)
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>Button clicked {count} times</p>
+        <button onclick={buttonClicked}></button>
     </div>
   )
 }
